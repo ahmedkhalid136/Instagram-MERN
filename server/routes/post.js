65cc9b94e0b7c1c680eb4a61ce13557cabc9a823
Ahmed <ahmedkhalid.hta@gmail.com>
2020-07-14 11:01:55 +0500

    Created All post route
    All post route is created with get request on postman

--- server/routes/post.js
+++ server/routes/post.js
@@ -8,6 +8,17 @@ const requireLogin = require("../middleware/requireLogin");
 const Post = mongoose.model("Post")
 
 
+router.get("/allpost",(req,res)=>{
+    Post.find()
+    .populate("postedBy","_id name")//all the post
+    .then(posts=>{
+        res.json(posts)
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
 router.post("/createpost",requireLogin,(req,res)=>{
     const {title,body} = req.body;
     if(!title || !body)
