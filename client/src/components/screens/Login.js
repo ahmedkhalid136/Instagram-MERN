23d243c810383e2446dcd4b07b783b86b8f75c88
Ahmed <ahmedkhalid.hta@gmail.com>
2020-07-15 16:36:59 +0500

    Created UI for login and signup
    Created UI and signup by using materializecss website

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
 const Login = ()=>{
     return(
-        <h1>Login</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="email" />
+                <input type="text" placeholder="password" />
+                <button className="btn waves-effect waves-light #1e88e5 blue darken-1" >
+                    Login
+                </button>
+                <h6>
+                    <Link to="./signup">Dont have an account ?</Link>
+                </h6>
+            </div>
+        </div>
     )
 }
 
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Signup = ()=>{
     return(
-        <h1>Signup</h1>
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+
+                <input type="text" placeholder="name" />
+                <input type="text" placeholder="email" />
+                <input type="text" placeholder="password" />
+                <button className="btn waves-effect waves-light #1e88e5 blue darken-1" >
+                    SignUP
+                </button>
+                <h6>
+                    <Link to="./login">Already have a account ?</Link>
+                </h6>
+            </div>
+        </div>
     )
 }
 
